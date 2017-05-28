81720d379e0e65cf9fadb0122573593344cc624f
Nikita Ermolenko <gnod94@gmail.com>
2017-05-28 21:29:51 +0600

    Initial parse logic for getting `type` and `name`

--- XNit/SourceEditorCommand.swift
+++ XNit/SourceEditorCommand.swift
@@ -12,9 +12,48 @@ import XcodeKit
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
-        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        for selection in invocation.buffer.selections {
+            
+            let range = selection as! XCSourceTextRange
+            var selectedLines = [String]()
+            
+            let startLine = range.start.line
+            let endLine = range.end.line
+            
+            for index in startLine...endLine {
+                let text = invocation.buffer.lines[index] as! String
+                // check empty string
+                selectedLines.append(text)
+            }
+            
+            // let, var
+            // :String, : String
+            // let `let`: Int
+            // var 2 = 4 !!!
+            
+            var properties = [(String, String)]() // name, type
+            let trimmingSet = CharacterSet(charactersIn: " ")
+            
+            for line in selectedLines {
+                if line.contains("=") {
+                    continue
+                }
+                let trimmedLine = line.trimmingCharacters(in: trimmingSet)
+                let components = trimmedLine.components(separatedBy: ":")
+                
+                guard let nameComponent = components.first, let typeComponent = components.last, components.count == 2 else {
+                    continue
+                }
+                    
+                let type = typeComponent.trimmingCharacters(in: trimmingSet)
+                
+                let startIndex = nameComponent.index(nameComponent.startIndex, offsetBy: 3)
+                let name = nameComponent[startIndex..<nameComponent.endIndex]
+                
+                print("name: \(name), type:\(type)")
+            }
+        }
         
         completionHandler(nil)
     }
-    
 }
