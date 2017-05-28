5b7b5d3ccf1cb7b7696e051021c904dd97ac67d9
Nikita Ermolenko <gnod94@gmail.com>
2017-05-28 23:38:00 +0600

    Improve init generation

--- XNit/SourceEditorCommand.swift
+++ XNit/SourceEditorCommand.swift
@@ -9,61 +9,79 @@
 import Foundation
 import XcodeKit
 
+extension String {
+    var indentation: String {
+        var indentation = ""
+        for character in characters {
+            if character == " " {
+                indentation.append(" ")
+            }
+            else {
+                break
+            }
+        }
+        return indentation
+    }
+}
+
+func *(count: Int, value: String) -> String {
+    var value = ""
+    for _ in 0...count {
+        value += " "
+    }
+    return value
+}
+
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
-        var properties = [(String, String)]() // name, type
+        let buffer = invocation.buffer
         
+        var properties = [(String, String)]() // name, type
+        var selectedLines = [String]()
+
         for selection in invocation.buffer.selections {
-            
             let range = selection as! XCSourceTextRange
-            var selectedLines = [String]()
-            
             let startLine = range.start.line
             let endLine = range.end.line
             
             for index in startLine...endLine {
                 let text = invocation.buffer.lines[index] as! String
-                // check empty string
+                if text.contains("=") || text.isEmpty {
+                    continue
+                }
+                guard text.contains("var") || text.contains("let") else {
+                    continue
+                }
                 selectedLines.append(text)
             }
+        }
+        
+        let trimmingSet = CharacterSet.whitespacesAndNewlines
+    
+        for line in selectedLines {
+            let trimmedLine = line.trimmingCharacters(in: trimmingSet)
+            let components = trimmedLine.components(separatedBy: ":")
             
-            // let, var
-            // :String, : String
-            // let `let`: Int
-            // var 2 = 4 !!!
- 
-            let trimmingSet = CharacterSet.whitespacesAndNewlines
-            
-            for line in selectedLines {
-                if line.contains("=") {
-                    continue
-                }
-                guard line.contains("var") || line.contains("let") else {
-                    continue
-                }
-                
-                let trimmedLine = line.trimmingCharacters(in: trimmingSet)
-                let components = trimmedLine.components(separatedBy: ":")
-                
-                guard let nameComponent = components.first, let typeComponent = components.last, components.count == 2 else {
-                    continue
-                }
-                    
-                let type = typeComponent.trimmingCharacters(in: trimmingSet)
-                
-                let startIndex = nameComponent.index(nameComponent.startIndex, offsetBy: 4)
-                let name = nameComponent[startIndex..<nameComponent.endIndex]
-                
-                properties.append(name, type)
+            guard let nameComponent = components.first, let typeComponent = components.last, components.count == 2 else {
+                continue
             }
+            
+            let type = typeComponent.trimmingCharacters(in: trimmingSet)
+            let startIndex = nameComponent.index(nameComponent.startIndex, offsetBy: 4)
+            let name = nameComponent[startIndex..<nameComponent.endIndex]
+            
+            properties.append(name, type)
         }
         
         guard properties.count > 0 else {
             return
         }
         
-        var initializer = "init("
+        let outsideIndentation = selectedLines.first!.indentation
+        let innerIndentation = outsideIndentation + (buffer.usesTabsForIndentation ? buffer.tabWidth : buffer.indentationWidth) * " "
+        
+        var initializer = outsideIndentation + "init("
         for (index, (name, type)) in properties.enumerated() {
             initializer.append("\(name): \(type)")
             
@@ -74,14 +92,11 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 
         initializer.append(") {\n")
         
-        for (index, (name, _)) in properties.enumerated() {
-            initializer.append("self.\(name) = \(name)")
-            
-            if index < properties.count - 1 {
-                initializer.append("\n")
-            }
+        for (name, _) in properties {
+            initializer.append(innerIndentation + "self.\(name) = \(name)")
+            initializer.append("\n")
         }
-        initializer.append("\n}\n")
+        initializer.append(outsideIndentation + "}\n")
         print(initializer)
         
         completionHandler(nil)
