bbcdd035c43e8b50f3750c833554ebb97baf9b39
Nikita Ermolenko <gnod94@gmail.com>
2017-05-28 22:32:15 +0600

    Generate a simple `init` without indentation now

--- XNit/SourceEditorCommand.swift
+++ XNit/SourceEditorCommand.swift
@@ -12,6 +12,8 @@ import XcodeKit
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        var properties = [(String, String)]() // name, type
+        
         for selection in invocation.buffer.selections {
             
             let range = selection as! XCSourceTextRange
@@ -30,14 +32,17 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             // :String, : String
             // let `let`: Int
             // var 2 = 4 !!!
-            
-            var properties = [(String, String)]() // name, type
-            let trimmingSet = CharacterSet(charactersIn: " ")
+ 
+            let trimmingSet = CharacterSet.whitespacesAndNewlines
             
             for line in selectedLines {
                 if line.contains("=") {
                     continue
                 }
+                guard line.contains("var") || line.contains("let") else {
+                    continue
+                }
+                
                 let trimmedLine = line.trimmingCharacters(in: trimmingSet)
                 let components = trimmedLine.components(separatedBy: ":")
                 
@@ -47,12 +52,37 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
                     
                 let type = typeComponent.trimmingCharacters(in: trimmingSet)
                 
-                let startIndex = nameComponent.index(nameComponent.startIndex, offsetBy: 3)
+                let startIndex = nameComponent.index(nameComponent.startIndex, offsetBy: 4)
                 let name = nameComponent[startIndex..<nameComponent.endIndex]
                 
-                print("name: \(name), type:\(type)")
+                properties.append(name, type)
             }
         }
+        
+        guard properties.count > 0 else {
+            return
+        }
+        
+        var initializer = "init("
+        for (index, (name, type)) in properties.enumerated() {
+            initializer.append("\(name): \(type)")
+            
+            if index < properties.count - 1 {
+                initializer.append(", ")
+            }
+        }
+
+        initializer.append(") {\n")
+        
+        for (index, (name, _)) in properties.enumerated() {
+            initializer.append("self.\(name) = \(name)")
+            
+            if index < properties.count - 1 {
+                initializer.append("\n")
+            }
+        }
+        initializer.append("\n}\n")
+        print(initializer)
         
         completionHandler(nil)
     }
